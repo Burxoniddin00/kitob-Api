6b110411c034643dcf4f4bbb7555a0499779cc3f
Burxoniddin00 <urxstvoldi@gmail.com>
2024-01-25 12:13:52 +0500

    .

--- src/db/mongoos.js
+++ src/db/mongoos.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 mongoose
-  .connect("mongodb://localhost:27017/Books")
+  .connect("mongodb://127.0.0.1:27017/Books")
   .then(console.log("connection"))
   .catch((er) => console.log(er.message));
