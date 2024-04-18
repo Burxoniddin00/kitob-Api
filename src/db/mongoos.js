2a718e3bc891439ca5fe54299adb9104e644d3f0
Burxoniddin00 <urxstvoldi@gmail.com>
2024-04-18 14:29:16 +0500

    .

--- src/db/mongoos.js
+++ src/db/mongoos.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 mongoose
   .connect("mongodb+srv://burxstvoldi:6kaADtAPMwEqBZuQ@cluster0.7inmvfr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   .then(console.log("connection"))
-  .catch((er) => console.log(er.message+'555'));
+  .catch((er) => console.log(er.message));
 
   
